2299c2c31de3b8082df3d6776224d0b8d7a40488
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-03 14:45:49 -0300

    DepartmentList controller

---
+++ src/gui/DepartmentListViewController.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.entities.Department;
+
+public class DepartmentListViewController implements Initializable {
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button BtNew;
+	
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+	}
+	
+
+}

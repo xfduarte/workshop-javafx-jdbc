85777820741211d9d0ffdf74d285258709d8843e
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-03 15:22:40 -0300

    DepartmentService

--- src/gui/DepartmentListViewController.java
+++ src/gui/DepartmentListViewController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,9 +13,12 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListViewController implements Initializable {
 
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	
@@ -25,6 +31,8 @@ public class DepartmentListViewController implements Initializable {
 	@FXML
 	private Button BtNew;
 	
+	private ObservableList<Department> obsList;
+	
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
@@ -33,11 +41,22 @@ public class DepartmentListViewController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
 
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
 	}
 	
-
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
 }

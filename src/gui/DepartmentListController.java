a884e3973d775f03e6ed0dbee31c620f9e273dc6
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-04 15:32:44 -0300

    Saving a new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -79,6 +79,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(service);
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -11,7 +11,14 @@ public class DepartmentService {
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
 	public List<Department> findAll() {
-
 		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}else {
+			dao.update(obj);
+		}
 	}
 }
